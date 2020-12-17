0de9b4db9bfed6ed45b882edabaad1970f92d7cb
Daniel <brikbusters@gmail.com>
2020-12-17 11:06:00 -0500

    Initial

---
+++ main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "header.h"
+
+#define LIMIT 100
+
+// Pretty print word wrap function I wrote for
+// https://code.heb12.com/heb12/heb12cli/src/branch/master/app.c
+void printBreak(char string[]) {
+	int breakAt = LIMIT;
+	for (int c = 0; string[c] != '\0'; c++) {
+		putchar(string[c]);
+		if (c > breakAt && string[c] == ' ') {
+			putchar('\n');
+			breakAt += LIMIT;
+		}
+	}
+
+	putchar('\n');
+}
+
+int main(int argc, char *argv[]) {
+	size_t article = 0;
+	size_t paragraph = 0;
+	size_t amendment = 0;
+	size_t *toset = &article;
+
+	// Parse command line arguments
+	for (int i = 1; i < argc; i++) {
+		if (argv[i][0] == '-' && argv[i][1] != '\0') {
+			// Set the pointer
+			switch (argv[i][1]) {
+			case 'a':
+				toset = &amendment;
+				break;
+			case 'p':
+				toset = &paragraph;
+				break;
+			case 't':
+				toset = &article;
+				break;
+			}
+
+			// Skip..
+			continue;
+		}
+
+		// Next parameter is the number, parse it
+		size_t c = 0;
+		while (isdigit(argv[i][c])) {
+			*toset *= 10;
+			*toset += (size_t)argv[i][c] - '0';
+			c++;
+		}
+	}
+
+	// Print article or amendment
+	if (article != 0) {
+		// Use pointer to select location of chapter
+		char **articlep;
+		switch (article) {
+		case 1: articlep = constitution.article_1; break;
+		case 2: articlep = constitution.article_2; break;
+		case 3: articlep = constitution.article_3; break;
+		case 4: articlep = constitution.article_4; break;
+		case 5: articlep = constitution.article_5; break;
+		case 6: articlep = constitution.article_6; break;
+		}
+
+		if (paragraph != 0) {
+			printBreak(articlep[paragraph - 1]);
+		} else {
+			size_t length = sizeof(**articlep) / sizeof(articlep[0]);
+			printf("%zu", length);
+			for (size_t i = 0; i < length; i++) {
+				printBreak(articlep[i]);
+			}
+		}
+	} else if (amendment != 0) {
+		printBreak(constitution.amendments[paragraph - 1]);
+	}
+}
+
